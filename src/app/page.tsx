80580bc4ea1af2de72da764498e222cb8b17e6c9
IXYOZ <nuttapon9.kn@gmail.com>
2025-08-28 20:02:54 +0930

    Test deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline text-blue-500"> Flexserv works!</h1>
+    <main className="flex h-screen items-center justify-center bg-green-100">
+     <h1 className="text-3xl font-bold underline text-blue-500"> Flexserv works!</h1>
+    </main>
   );
 }
