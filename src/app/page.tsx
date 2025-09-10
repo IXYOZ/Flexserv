eb79753aeacb386533ad82b09d73cbb87aa608de
IXYOZ <nuttapon9.kn@gmail.com>
2025-09-10 11:43:54 +0930

    Direct home to feed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
   'use client'
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 
 
 export default function Home() {
+
+  const router = useRouter()
   return (
     <div className="p-4">
-      <p>Promotion ETC...</p>
+      <p>Promotion ETC... </p>
+      <button onClick={() => router.push('/feed')} className="bg-white hover:bg-blue-500 hover:text-white rounded p-1">Go to feed</button>
     </div>
   );
 }
